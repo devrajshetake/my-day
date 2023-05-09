cd9f64a1fbf368ae90bafa376ef74bae3b7445dd
Devraj Shetake <devrajshetake@gmail.com>
2023-05-09 15:09:42 +0530

    filter

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -9,20 +9,51 @@ import { firstValueFrom } from 'rxjs';
 })
 
 export class NewsComponent implements OnInit {
-  articles: any;
+  articles: any[] = [];
+  countries: any[] = [['in', 'India'], ['us', 'United States'], ['gb', 'Great Britain'], ['au', 'Australia'], ['ca', 'Canada']];
+  filterCountries: string[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getNews().subscribe((data: any) => {
+    this.articles = [];
+    for(let country of this.filterCountries){
+    this.getNews(country).subscribe((data: any) => {
       console.log(data);
-      this.articles = data.articles;
+      this.articles = [...this.articles, ...data.articles];
     });
   }
+  }
 
-  getNews() {
+  getNews(code:string) {
     const apiKey = '7987bfcc2d3a4ba7babf296323edd56b';
-    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${code}&apiKey=${apiKey}`;
+    console.log(url)
     return this.http.get(url);
+  }
+
+  toggleFilter(event: any) {
+    const checkboxValue = event.target.value;
+    if (event.target.checked) {
+      // Add the value to the array of selected countries
+      this.filterCountries.push(checkboxValue);
+    } else {
+      // Remove the value from the array of selected countries
+      this.filterCountries = this.filterCountries.filter(c => c !== checkboxValue);
+    }
+
+    this.getNewArticles();
+  }
+
+  getNewArticles(){
+    this.articles = []
+    for(let country of this.filterCountries){
+      this.getNews(country).subscribe((data: any) => {
+        console.log(data);
+        this.articles = [...this.articles, ...data.articles];
+      });
+    }
+
+    this.articles.sort(() => Math.random() - 0.5);
   }
 }
